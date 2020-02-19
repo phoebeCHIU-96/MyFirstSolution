6948425432285a2cde3b2d1d0c5ccb6665d3b28b
unknown <phoebe795@gmail.com>
2020-02-19 16:00:29 +0800

    加入專案檔案。

---
+++ ConditionDisplay/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionDisplay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int condition = 10;
+            int value;
+            Console.WriteLine("請輸入一個數字:");
+            string input = Console.ReadLine();
+            value = int.Parse(input);
+            if (value > condition)
+            {
+                Console.WriteLine($"輸入的值大於{condition}"); 
+            }
+            else
+            {
+                Console.WriteLine($"輸入的值小於{condition}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
